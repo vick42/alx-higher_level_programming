e71c11f363a17c4a7102e81b8311fbfc152bd775
Victor <vicmwiti@gmail.com>
2023-01-11 16:54:44 -0800

    add all tasks

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Attribute adding module"""
+
+
+def add_attribute(a_class, name, value):
+    """Adds a new attribute to an object if it's possible"""
+    if hasattr(a_class, "__dict__"):
+        setattr(a_class, name, value)
+    else:
+        raise TypeError("can't add new attribute")
