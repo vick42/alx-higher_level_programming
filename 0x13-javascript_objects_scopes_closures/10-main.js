be22f700998dcf109f86900a6379da019311c476
Victor Mwiti <vicmwiti@gmail.com>
2023-04-29 15:52:38 -0700

    semistandard compliance testing

--- 0x13-javascript_objects_scopes_closures/10-main.js
+++ 0x13-javascript_objects_scopes_closures/10-main.js
@@ -7,10 +7,8 @@ console.log(myConverter(2));
 console.log(myConverter(12));
 console.log(myConverter(89));
 
-
 myConverter = converter(16);
 
 console.log(myConverter(2));
 console.log(myConverter(12));
 console.log(myConverter(89));
-
