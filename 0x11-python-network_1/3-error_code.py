53372364a66854340514d7a793aa4b2fd9ceff59
Victor Mwiti <vicmwiti@gmail.com>
2023-05-19 15:54:47 -0700

    review of task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,16 +3,20 @@
 Usage: ./3-error_code.py <URL>
   - Handles HTTP errors.
 """
-import sys
-import urllib.error
 import urllib.request
+import urllib.parse
+import urllib.error
+import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
 
-    request = urllib.request.Request(url)
+def main(url,):
     try:
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print("Error code: []".format(e.code))
+        print('Error code: {}'.format(e.code))
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    main(url)
