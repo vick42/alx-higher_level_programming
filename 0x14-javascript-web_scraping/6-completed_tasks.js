45d196add748f805ddd8adf37aa484a68cb4f05b
vick42 <vicmwiti@gmail.com>
2024-07-31 10:53:51 -0700

    new task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,24 +2,21 @@
 
 const request = require('request');
 const url = process.argv[2];
-
-request(url, function (err, response, body) {
-	if (err) {
-		console.log(err);
-	} else if (response.statusCode === 200) {
-		const completed = {};
-		const tasks = JSON.parse(body);
-		for (const i in tasks) {
-			const task = tasks[i];
-			if (completed[task.userId] === undefined) {
-				completed[task.userId] = 1;
-			} else {
-				completed[task.userId]++;
-			}
-		}
-	}
-	console.log(comleted);
-	} else {
-		console.log('An error occured. Status code: ' + response.statusCode);
-	}
+const list = {};
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const abc = JSON.parse(body);
+    abc.forEach(function (item, index, array) {
+      if (item.completed) {
+        if (list[item.userId] === undefined) {
+          list[item.userId] = 1;
+        } else {
+          list[item.userId]++;
+        }
+      }
+    });
+    console.log(list);
+  }
 });
